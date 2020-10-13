f3f0f376ac7a966588fc4bf41cd01c35dc7ddb3d
ajakcyer <ajakcyer97@gmail.com>
2020-10-13 09:59:36 -0400

    working on submiting form

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,28 @@
 class CompaniesController < ApplicationController
+
+    def show
+        @company = Company.find(params[:id])
+    end
+
+    def new
+        @company = Company.new
+    end
+
+    def create
+        byebug
+        company = Company.create(company_params)
+
+        if company.valid?
+            redirect_to company_path(company)
+        else
+            flash[:my_errors] = company.errors.full_messages
+            redirect_to new_company_path
+        end
+    end
+
+    private
+
+    def company_params
+        params.require(:company).permit(:name, offices_attributes: [:id, offices: []])
+    end
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,17 @@
 class Company < ApplicationRecord
+    has_many :offices
+    has_many :buildings, through: :offices
+    has_many :employees
+
+    validates :name, presence: true
+
+    accepts_nested_attributes_for :offices
+
+    # def offices_attributes=(office_attributes)
+    #     byebug
+    #     office_attributes.values.each do |attribute|
+    #         Building.find(attribute[:id])
+    #     end
+    # end
+
 end
